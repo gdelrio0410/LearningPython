82dde482522747c86ddc2575e0256778c6be3918
Guillermo <gdelrio0410@gmail.com>
2020-10-07 13:37:01 -0500

    tipos de datos

---
+++ cursoUno/variables-y-tipos/tipos.py
@@ -0,0 +1,84 @@
+nada = None
+cadena = "hola men llamao Guillermo"
+entero = 578
+flotante = 45.23
+boolean = True
+lista = [10, 20, 30, 100, 548631]
+listaString = [44,"treinta", 523.12, None, 5464 , "ooooh noo!!"]
+#lista q no cambia
+tupla = ("apendiendo ", "a", "usar", "python")
+diccionario = {
+    "nombre": "guillermo",
+    "apellido": "Del Rio",
+    "curso": "python"
+}
+rango = range(9)
+dato_byte = b"probando"
+
+
+#imprimir variable
+print(nada)
+#mostrar el tipo de dato
+print(type(nada))
+print("###########")
+
+#imprimir variable
+print(cadena)
+#mostrar el tipo de dato
+print(type(cadena))
+print("###########")
+
+#imprimir variable
+print(entero)
+#mostrar el tipo de dato
+print(type(entero))
+print("###########")
+
+#imprimir variable
+print(flotante)
+#mostrar el tipo de dato
+print(type(flotante))
+print("###########")
+
+#imprimir variable
+print(boolean)
+#mostrar el tipo de dato
+print(type(boolean))
+print("###########")
+
+#imprimir variable
+print(lista)
+#mostrar el tipo de dato
+print(type(lista))
+print("###########")
+
+#imprimir variable
+print(listaString)
+#mostrar el tipo de dato
+print(type(listaString))
+print("###########")
+
+#imprimir variable
+print(tupla)
+#mostrar el tipo de dato
+print(type(tupla))
+print("###########")
+
+#imprimir variable
+print(diccionario)
+#mostrar el tipo de dato
+print(type(diccionario))
+print("###########")
+
+#imprimir variable
+print(rango)
+#mostrar el tipo de dato
+print(type(rango))
+print("###########")
+
+#imprimir variable
+print(dato_byte)
+#mostrar el tipo de dato
+print(type(dato_byte))
+print("###########")
+
