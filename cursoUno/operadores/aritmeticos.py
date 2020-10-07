c47154fbd8d019032a347f7c018debffa9c040da
Guillermo <gdelrio0410@gmail.com>
2020-10-07 14:06:37 -0500

    operaciones aritmeticas

---
+++ cursoUno/operadores/aritmeticos.py
@@ -0,0 +1,14 @@
+numero1 = 77
+numero2 = 852
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+dividir = numero1 / numero2
+modulo = numero1%numero2
+
+print("+++++++++Calculadora+++++++++")
+print(f"la suma es de: {numero1 + numero2}")
+print(f"la resta es de: {resta}")
+print(f"la multiplicacion es de: ", multiplicacion)
+print(f"la divicion es de: ", dividir)
+print(f"la modulo es de: ", modulo)
