3eff03383b941cdceb6aac375820b4fd808aaf56
Guillermo <gdelrio0410@gmail.com>
2020-10-07 13:57:22 -0500

    curiosidades

---
+++ cursoUno/variables-y-tipos/curiosidades.py
@@ -0,0 +1,19 @@
+texto1 = "aprendiendo"
+texto2 = " python"
+
+texto_unido = texto1 + " " + texto2
+print(texto_unido)
+
+
+
+texto1 = "aprendiendo a"
+texto2 = " usar \"python\""
+
+texto_unido = texto1 + " " + texto2
+print(texto_unido)
+texto_unido = texto1 + " \n " + texto2
+print(texto_unido)
+texto_unido = texto1 + " \t " + texto2
+print(texto_unido)
+texto_unido = texto1 + " \r " + texto2
+print(texto_unido)
