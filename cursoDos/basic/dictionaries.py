cbd2f1ca078298c8378d122dc2e308e712073d57
Guillermo <gdelrio0410@gmail.com>
2020-10-07 21:04:53 -0500

    diccionaries y tuples

---
+++ cursoDos/basic/dictionaries.py
@@ -0,0 +1,26 @@
+d = {"key1":"value1","key2":"value2"}
+print(d)
+print(d["key2"])
+
+d["key3"] = "value3"
+print(d)
+
+code_names = {
+    "obama":"renegade",
+    "bush":"trailblazer",
+    "reagan":"rawhide",
+    "ford":"passkey"
+}
+
+print(code_names["bush"])
+
+population ={
+    "usa":323,
+    "germany":85,
+    "india":1234
+}
+print(population["india"])
+
+print(code_names.keys())
+print(code_names.values())
+print(code_names.items())
---
+++ cursoDos/basic/tuples.py
@@ -0,0 +1,3 @@
+tuples = (1,2,3)
+print(tuples.count(2))
+print(tuples.index(3))
